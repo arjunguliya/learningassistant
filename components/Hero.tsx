6b46099a86712d2413608df6d7d5ce17203baf50
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-06-27 18:05:39 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,7 +45,7 @@ const Hero: FC<THeroProps> = ({
             Powered by <b>Scholar AI</b>
           </span>
         </a>
-        <h2 className="mt-2 bg-custom-gradient bg-clip-text text-center text-4xl font-medium tracking-tight text-gray-900 sm:text-6xl">
+        <h2 className="mt-2 bg-custom-gradient bg-clip-text text-center text-3xl font-medium tracking-tight text-gray-900 sm:text-4xl">
           Your Personal{" "}
           <span className="bg-gradient-to-r from-blue-600 to-blue-500 bg-clip-text font-bold text-transparent">
             AI Learning Assistant
