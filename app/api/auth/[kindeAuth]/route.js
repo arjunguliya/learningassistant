36e1a6dfdbdb1761d75bc5557109d6e9c18f278c
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-06-29 22:47:45 +0530

    Update route.js

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,3 +1,9 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const GET = handleAuth();
+
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+<LoginLink>Sign in</LoginLink>
+
+<RegisterLink>Sign up</RegisterLink>
