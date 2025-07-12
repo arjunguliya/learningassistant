57cc903f91429a085658e03387f0f2efefdd142d
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-07-12 22:53:04 +0530

    Delete app/api/auth/[kindeAuth]/route_old.js

--- app/api/auth/[kindeAuth]/route_old.js
+++
@@ -1,9 +0,0 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-
-export const GET = handleAuth();
-
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-
-<LoginLink>Sign in</LoginLink>
-
-<RegisterLink>Sign up</RegisterLink>
