b6df6c398e3d04d18b8549867417d8215054b68f
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-07-12 23:30:13 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
-import { Logo } from "./logo";
 import { LoginLink, RegisterLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Image from "next/image";
 
 const Header = async () => {
   const { isAuthenticated, getUser } = getKindeServerSession();
@@ -9,13 +9,20 @@ const Header = async () => {
   return (
     <div className="container flex h-[60px] shrink-0 items-center justify-between px-4 lg:h-[80px] lg:px-0">
       <a href="/">
-        <Logo className="w-30 sm:w-36" />
+        <Image
+          unoptimized
+          src="/new-logo.svg"
+          alt="Logo"
+          width={120}
+          height={36}
+          className="w-30 sm:w-36"
+        />  
       </a>
       
       <div className="flex items-center gap-4">
         {!(await isAuthenticated()) ? (
           <>
-            <LoginLink className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700">
+            <LoginLink className="rounded-md [#5170ff] px-4 py-2 text-sm font-medium text-white hover:[#4c63d2]">
               Sign in
             </LoginLink>
             <RegisterLink className="rounded-md border border-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
