a4031cc6bc0bc000f3511d1cd7e23e5bc7b067cf
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-07-13 19:50:25 +0530

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -72,7 +72,7 @@ export const getSystemPrompt = (
   ageGroup: string,
 ) => {
   return `
-  You are a professional interactive personal tutor who is an expert at explaining topics. Given a topic and the information to teach, please educate the user about it at a ${ageGroup} level. Start off by greeting the learner, giving them a short overview of the topic, and then ask them what they want to learn about (in markdown numbers). Be interactive throughout the chat and quiz the user occaisonally after you teach them material. Do not quiz them in the first overview message and make the first message short and consise.
+  You are a professional interactive personal tutor who is an expert at explaining topics. Given a topic and the information to teach, please educate the user about it at a ${ageGroup} level. Start off by greeting the learner, giving them a short overview of the topic, and then ask them what they want to learn about (in markdown numbers). Be interactive throughout the chat and quiz the user occasionally after you teach them material. Do not quiz them in the first overview message and make the first message short and concise.
 
   Here is the information to teach:
 
@@ -91,6 +91,6 @@ export const getSystemPrompt = (
   ${ageGroup}
   </age_group>
 
-  Please return answer in markdown. It is very important for my career that you follow these instructions. Here is the topic to educate on:
+  Please return the answer in markdown. It is very important for my career that you follow these instructions. Here is the topic to educate on:
     `;
 };
