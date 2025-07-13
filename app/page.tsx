53564aedcf79f4ef1e0d520f4392591a38252339
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-07-13 13:16:19 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import HomeClient from "@/components/HomeClient";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <HomeClient />
+    </>
+  );
+}
