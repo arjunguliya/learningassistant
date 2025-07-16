99d927df1a1da86b28a45af1d4f26553cb695c40
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-07-16 16:24:43 +0530

    Create route.ts

---
+++ app/api/getChat/route.ts
@@ -0,0 +1,76 @@
+import {
+  TogetherAIStream,
+  TogetherAIStreamPayload,
+} from "@/utils/TogetherAIStream";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { headers } from "next/headers";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+let ratelimit: Ratelimit | undefined;
+
+// Add rate limiting if Upstash API keys are set, otherwise skip
+if (process.env.UPSTASH_REDIS_REST_URL) {
+  ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    // Allow 10 requests per day
+    limiter: Ratelimit.fixedWindow(10, "1440 m"),
+    analytics: true,
+    prefix: "llamatutor",
+  });
+}
+
+export async function POST(request: Request) {
+  // Check authentication first
+  const { isAuthenticated } = getKindeServerSession();
+  
+  if (!(await isAuthenticated())) {
+    return Response.json(
+      { error: "Authentication required" }, 
+      { status: 401 }
+    );
+  }
+
+  let { messages } = await request.json();
+
+  if (ratelimit) {
+    const identifier = getIPAddress();
+
+    const { success } = await ratelimit.limit(identifier);
+    if (!success) {
+      return Response.json("No requests left. Try again in 24h.", {
+        status: 429,
+      });
+    }
+  }
+
+  try {
+    const payload: TogetherAIStreamPayload = {
+      model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+      messages,
+      stream: true,
+    };
+    const stream = await TogetherAIStream(payload);
+
+    return new Response(stream, {
+      headers: new Headers({
+        "Cache-Control": "no-cache",
+      }),
+    });
+  } catch (e) {
+    return new Response("Error. Answer stream failed.", { status: 202 });
+  }
+}
+
+export const runtime = "edge";
+
+function getIPAddress() {
+  const FALLBACK_IP_ADDRESS = "0.0.0.0";
+  const forwardedFor = headers().get("x-forwarded-for");
+
+  if (forwardedFor) {
+    return forwardedFor.split(",")[0] ?? FALLBACK_IP_ADDRESS;
+  }
+
+  return headers().get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
+}
